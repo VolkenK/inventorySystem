8f00d3b4ed3fd51355b2e2372212b0451e292f5d
VolkenK <volkenkoelewijn@gmail.com>
2018-10-08 10:16:29 +0200

    Add files via upload

---
+++ Scripts/PlayerCollisionr.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionr : MonoBehaviour {
+    [SerializeField]
+    private Inventory inventory;
+
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.GetComponent<Item>() != null)
+        {
+            inventory.AddToInventory(collision.gameObject);
+        }
+    }
+}
